ce34e3aaaa9fca3857932f3c3d934650f4f9a2f5
Mikael Håkansson <wmmihaa@hotmail.com>
2017-06-22 08:31:46 +0200

    ***

---
+++ CITest.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace functionciapp
+{
+    public static class CITest
+    {
+        [FunctionName("CITestFunction")]
+        public static void Run([ServiceBusTrigger("%testqueue%", AccessRights.Manage, Connection = "SBConnectionString")]BrokeredMessage myQueueItem, TraceWriter log)
+        {
+            log.Info($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
